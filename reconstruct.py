09f1efe5f6519227200b23a752bdf4b15bcc2a2e
jiyun.park <laurenceyoon@gmail.com>
2023-09-24 20:47:33 +0900

    update transpose & normalize rms audio

--- reconstruct.py
+++ reconstruct.py
@@ -22,6 +22,10 @@ SINGERS = {
 }  # singer_name: role
 SONG_IDS = ["{:02d}".format(i) for i in range(1, 25)]  # 01 ~ 24
 
+# Audio parameters
+SAMPLE_RATE = 16000
+HOP_LENGTH = 640  # 40ms
+
 
 def make_empty_directory(dest_dir):
     (dest_dir / AUDIO_DIR).mkdir(parents=True, exist_ok=True)
@@ -117,45 +121,75 @@ def filter_voice_note_ann(src_dir, dest_dir):
 def get_semitone_difference(key1, key2):
     midi_num1 = librosa.note_to_midi(key1.split(":")[0])
     midi_num2 = librosa.note_to_midi(key2.split(":")[0])
-    return midi_num2 - midi_num1
+    return midi_num1 - midi_num2
 
 
 def transpose_audio_keys(dest_dir):
     # Read the CSV file & filter out the ref & target rows
-    audio_key_info = dict.fromkeys(SONG_IDS, dict())
+    audio_key_info = {}
     key_ann_csv_file = dest_dir / AUDIO_KEY_ANN_DIR / "ann_audio_globalkey-tuning.csv"
     with open(key_ann_csv_file, mode="r") as csv_file:
         reader = csv.DictReader(csv_file, delimiter=";")
-        for song_id in SONG_IDS:
-            for row in reader:
-                if row["WorkID"][-2:] != song_id:
-                    continue
-                if row["PerformanceID"] == "HU33":
-                    audio_key_info[song_id]["ref"] = row
-                elif row["PerformanceID"] == "SC06":
-                    audio_key_info[song_id]["target"] = row
+        for row in reader:
+            song_id = row["WorkID"][-2:]
+            if row["PerformanceID"] == "HU33":
+                if song_id not in audio_key_info.keys():
+                    audio_key_info[song_id] = {"ref": {}, "target": {}}
+                audio_key_info[song_id]["ref"] = row
+            elif row["PerformanceID"] == "SC06":
+                if song_id not in audio_key_info.keys():
+                    audio_key_info[song_id] = {"ref": {}, "target": {}}
+                audio_key_info[song_id]["target"] = row
 
     # transpose target audio keys to ref audio key
-    print("Transposing target audio keys to ref audio key...")
+    print("\nTransposing target audio keys to ref audio key...")
     for song_id in tqdm(SONG_IDS):
         target_audio = dest_dir / AUDIO_DIR / f"{FILENAME_PREFIX}{song_id}_target.wav"
-        print(f"Transposing target audio({target_audio})...")
-        target_y, sr = librosa.load(target_audio.as_posix(), sr=None)
+        target_y, _ = librosa.load(target_audio.as_posix(), sr=SAMPLE_RATE)
         ref_info, target_info = (
             audio_key_info[song_id]["ref"],
             audio_key_info[song_id]["target"],
         )
-        # print(f"[{song_id}] ref_info: {ref_info}, target_info: {target_info}")
         semitone_diff = get_semitone_difference(ref_info["Key"], target_info["Key"])
-        # print(f"[{song_id}] semitone_diff: {semitone_diff}")
+        print(
+            f"[{song_id}] semitone_diff: {semitone_diff}, ref_info: {ref_info}, target_info: {target_info}"
+        )
 
         # Transpose the target audio
         y_transposed = librosa.effects.pitch_shift(
-            target_y, sr=sr, n_steps=semitone_diff
+            target_y, sr=SAMPLE_RATE, n_steps=semitone_diff
         )
 
         # Save the transposed audio
-        sf.write(target_audio.as_posix(), y_transposed, sr)
+        sf.write(target_audio.as_posix(), y_transposed, SAMPLE_RATE)
+
+
+def normalize_audio_rms(dest_dir):
+    print(f"\nNormalizing audio RMS...")
+    for song_id in tqdm(SONG_IDS):
+        # load audio files
+        ref_audio_path = dest_dir / AUDIO_DIR / f"{FILENAME_PREFIX}{song_id}_ref.wav"
+        target_audio_path = (
+            dest_dir / AUDIO_DIR / f"{FILENAME_PREFIX}{song_id}_target.wav"
+        )
+        audio_file1, _ = librosa.load(ref_audio_path.as_posix(), sr=SAMPLE_RATE)
+        audio_file2, _ = librosa.load(target_audio_path.as_posix(), sr=SAMPLE_RATE)
+
+        # calculate RMS values
+        rms1 = librosa.feature.rms(
+            y=audio_file1, frame_length=HOP_LENGTH * 2, hop_length=HOP_LENGTH
+        )[0].max()
+        rms2 = librosa.feature.rms(
+            y=audio_file2, frame_length=HOP_LENGTH * 2, hop_length=HOP_LENGTH
+        )[0].max()
+
+        # normalize target audio file with ref audio file
+        audio_file2_normalized = audio_file2 * (rms1 / rms2)
+
+        # save normalized target audio file
+        sf.write(
+            target_audio_path.as_posix(), audio_file2_normalized, samplerate=SAMPLE_RATE
+        )
 
 
 def copy_from_orig_dataset(src_dir, dest_dir):
@@ -176,7 +210,8 @@ def main(src_dir, dest_dir):
     # transpose target audio keys to ref audio key
     transpose_audio_keys(dest_dir)
 
-    # Normalize the RMS of the audio files (TBD)
+    # Normalize the RMS of the audio files
+    normalize_audio_rms(dest_dir)
 
 
 if __name__ == "__main__":
